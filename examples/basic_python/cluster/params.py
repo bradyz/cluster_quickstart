20eb993a3b9b6b4aeed52273b59b690ac3bf7e62
bradyz <brady.zhou@utexas.edu>
2021-10-28 15:33:56 -0500

    update

--- examples/basic_python/cluster/params.py
+++ examples/basic_python/cluster/params.py
@@ -1,32 +1,30 @@
 import pathlib
 
 
-HEADER = """#!/bin/bash
+WORK_DIR = pathlib.Path(__file__).parent.parent
+TEMPLATE = f"""#!/bin/bash
 
 export SOME_VAR=123
 export PYTHONPATH=$PYTHONPATH:/scratch/cluster/user_id/some_path
 
 conda env list
-"""
+cd {WORK_DIR}
+
+python3 run.py """
 
 
 # Defines a grid search over these parameters.
 PARAMS = {
-        'arg1': [2020, 1337],
-        'arg2': ['foo', 'bar']
-        }
+    'arg1': [2020, 1337],
+    'arg2': ['foo', 'bar']
+}
 
 
 def get_job(arg1, arg2):
     """
     build your shell script here.
     """
-    shell_script_str = HEADER
-
-    # this file is /foo/bar/../basic_python/cluster/params.py
-    shell_script_str += 'cd %s\n' % pathlib.Path(__file__).parent.parent
-
-    # the script we want is /foo/bar/../basic_python/run.py
-    shell_script_str += 'python3 run.py %s %s' % (arg1, arg2)
+    shell_script_str = TEMPLATE
+    shell_script_str += f'{arg1} {arg2}'
 
     return shell_script_str
--- generate_slurm.py
+++ generate_slurm.py
@@ -5,22 +5,22 @@ from pathlib import Path
 
 SUBMIT = """#!/bin/bash
 
-#SBATCH --job-name {name}                    # Job name
+#SBATCH --job-name {name}                  # Job name
 
 ### Logging
 #SBATCH --output={log_dir}/{name}_%j.out   # Name of stdout output file (%j expands to jobId)
 #SBATCH --error={log_dir}/{name}_%j.err    # Name of stderr output file (%j expands to jobId)
-#SBATCH --mail-user=bzhou@cs.utexas.edu      # Email of notification
+#SBATCH --mail-user=someemail@mail.com     # Email of notification
 #SBATCH --mail-type=END,FAIL,REQUEUE
 
 ### Node info
-###SBATCH --partition Test                   # Queue name [NOT NEEDED FOR NOW]
-#SBATCH --nodes=1                            # Always set to 1 when using the cluster
-#SBATCH --time 78:00:00                      # Run time (hh:mm:ss)
-#SBATCH --ntasks-per-node=1                  # Number of tasks per node (Set to the number of gpus requested)
-#SBATCH --gres=gpu:1                         # Number of gpus needed
-#SBATCH --cpus-per-task=8                    # Number of cpus needed per task
-#SBATCH --mem=48G                            # Memory requirements
+###SBATCH --partition Test                 # Queue name [NOT NEEDED FOR NOW]
+#SBATCH --nodes=1                          # Always set to 1 when using the cluster
+#SBATCH --time 78:00:00                    # Run time (hh:mm:ss)
+#SBATCH --ntasks-per-node=1                # Number of tasks per node (Set to the number of gpus requested)
+#SBATCH --gres=gpu:1                       # Number of gpus needed
+#SBATCH --cpus-per-task=8                  # Number of cpus needed per task
+#SBATCH --mem=48G                          # Memory requirements
 
 ./{name}.sh
 """
